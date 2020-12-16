f7323efe93eae996635cef69f34037446fd7acb7
Aina Zuliana <66254639+ainazln@users.noreply.github.com>
2020-12-17 05:04:28 +0800

    Add files via upload

---
+++ ASSIGNMENT 1A - AINA ZULIANA ZULKEFLI - 2019324829/PACKAGE/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1584722149,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/logo-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/iceking-sheet0.png",
+		"images/player-sheet0.png",
+		"images/explode-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/hiclipartcom2-sheet0.png",
+		"images/finnjake-sheet0.png",
+		"images/finnjake-sheet1.png",
+		"media/sparkle.ogg",
+		"media/sparkle.m4a",
+		"media/big boy coconuts.m4a",
+		"media/big_boy_coconuts.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
---
+++ ASSIGNMENT 1B - AINA ZULIANA ZULKEFLI - 2019324829/PACKAGE/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1588972122,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/bg2-sheet0.png",
+		"images/poof-sheet0.png",
+		"images/pipebottom-sheet0.png",
+		"images/pipetop-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/bg3-sheet0.png",
+		"images/poof2-sheet0.png",
+		"media/poof.m4a",
+		"media/poof.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
